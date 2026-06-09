bcd44e1e3a8f7a6ebb9da4a89bd2654eb37f6542
JuniorKologne <jkologne85@gmail.com>
2026-06-09 11:51:38 +0200

    #2 Frontend initialisation de REACT

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+    return (
+        <div style={{ textAlign: 'center', marginTop: '40px' }}>
+            <h1> Frontend React operationnel</h1>
+             <p>Connectons nous sur React</p>
+        </div>
+    )
+}
